93fbc99a2137e28543987d3d6c802e726c3f7644
yossatorn <yossatorn2007@gmail.com>
2017-11-30 08:56:41 +0000

    1st

--- index.php
+++ index.php
@@ -23,5 +23,32 @@
 	<?php } else { ?>
 		<h2>MySQL Server version: <?php echo mysql_get_server_info(); ?></h2>
 	<?php } ?>
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "sceIk2heoaLN";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
 </body>
 </html>
